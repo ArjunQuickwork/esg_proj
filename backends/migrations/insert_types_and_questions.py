c1c8183c45763fde317a77a88029d4da24b20c76
Arjun113 <arjunsanghi2005@gmail.com>
2026-01-05 12:33:42 +0530

    fix: path broken in VM

--- backends/migrations/insert_types_and_questions.py
+++ backends/migrations/insert_types_and_questions.py
@@ -1,10 +1,14 @@
 import json
+from pathlib import Path
+
 from backends.constants.mongo_client import questions_collection, industries_collection
 
 def main() -> None:
-    with open("./consistent_question_types.json", 'r') as f:
+    base_dir = Path(__file__).parent
+
+    with open(base_dir / "consistent_question_types.json", 'r') as f:
         questions = json.load(f)
-    with open("./company_initial_types.json", 'r') as f:
+    with open(base_dir / "company_initial_types.json", 'r') as f:
         industries = json.load(f)
 
     questions_collection.insert_one(questions)
