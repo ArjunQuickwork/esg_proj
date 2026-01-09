99fae6af6f6f53a2a1f64b347ae2c3eb7d55b4b4
Arjun113 <arjun@quickwork.co>
2026-01-09 16:22:13 +0530

    added content for docs

--- docs/app/_meta.global.tsx
+++ docs/app/_meta.global.tsx
@@ -1,12 +1,38 @@
-import {MetaRecord} from "nextra";
+import { MetaRecord } from "nextra";
 
-export default {
+const API_PUBLIC_DOCS = {
+    index: "Overview",
+    //fetch_companies: "Fetch companies",
+    //fetch_company_data: "Fetch company data",
+    fetch_industries: "Fetch industries",
+    //fetch_companies_by_industry: "Fetch companies by industry",
+    //fetch_questions: "Fetch questions",
+};
+
+const API_DOCS = {
+    index: "API Overview",
+    public: {
+        title: "Public API",
+        type: "page",
+        items: API_PUBLIC_DOCS,
+    },
+};
+
+const meta = {
     index: {
-        type: 'page',
+        type: "page",
         theme: {
-            layout: 'full',
+            layout: "full",
             toc: false,
             timestamp: false,
-        }
-    }
-}
+        },
+    },
+
+    api: {
+        title: "API Reference",
+        items: API_DOCS,
+        type: "page",
+    },
+};
+
+export default meta;
